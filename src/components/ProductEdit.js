a710d4e21651e84a6fe72cb4655ba5ad6a366a8b
Lilla Varga <lillaev@yahoo.com>
2021-11-01 22:53:20 +0100

    10. Added dynamic table to edit page

--- src/components/ProductEdit.js
+++ src/components/ProductEdit.js
@@ -1,19 +1,45 @@
-import React from 'react';
-import { Column, Table2} from "@blueprintjs/table";
+import React, {useState} from 'react';
+import { HTMLTable} from "@blueprintjs/core";
 import styled from 'styled-components';
+import { useProduct } from '../contexts/ProductContext';
 
 
 const ProductEdit = () => {
-    
+
+    const {allProducts, setAllProducts} = useProduct();
+
+  
     return (
-    <Wrapper className="wrapper">
-        <h1>Edit Products</h1>
-        <StyledTable numRows={3}>
-            <Column className="col" name="name"/>
-            <Column className="col" name="price"/>
-            <Column className="col" name = "discount"/>
-        </StyledTable>
-    </Wrapper>
+    
+        <Wrapper className="wrapper">
+            <h1>Edit Products</h1>
+            <form action="">
+            <HTMLTable interactive={true}>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Description</th>
+                            <th>Price</th>
+                            <th>Discount</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {allProducts.map((product) => 
+                        <tr>
+                            <td>{product.name}</td>
+                            <td>{product.Description}</td>
+                            <td>{product.Price}</td>
+                            <td>{product.Discount}</td>
+                        </tr>
+                        )}
+                        
+                    </tbody>
+                </table>
+            </HTMLTable>
+            </form>
+        </Wrapper>
+    
     );
 }
 
@@ -22,16 +48,25 @@ max-width: 1200px;
 width:max-content;
 margin: 0px auto;
 padding:20px 20px 20px 250px;
+form {
+    max-width:700px;
+    width:100%;
+
+}
+table {
+    max-width: 100%;
+    margin: 25px auto;
+    tr {
+        max-width:100%;
+    }
+   
+}
 z-index: -10;
     @media (max-width:767px) {
         padding: 15px;
     }
-`
-const StyledTable = styled(Table2) `
-max-width: 100%;
-margin: 50px auto;
+`;
 
-`
 
 export default ProductEdit;
 
