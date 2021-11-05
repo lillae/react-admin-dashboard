90f83f048bb3de048d4ece336539c85c8b1d9e7f
Lilla Varga <lillaev@yahoo.com>
2021-11-06 00:24:46 +0100

    name input

--- src/components/products/ProductEdit.js
+++ src/components/products/ProductEdit.js
@@ -32,7 +32,7 @@ const ProductEdit = () => {
         setEditProductId(product.uuid);
 
         const formValues = {
-            name: product.Name,
+            name: product.name,
             Description: product.Description,
             Price: product.Price,
             Discount: product.Discount
@@ -45,7 +45,7 @@ const ProductEdit = () => {
         e.preventDefault();
 
         const editedProduct = {
-            name: editForm.Name,
+            name: editForm.name,
             Description: editForm.Description,
             Price: editForm.Price,
             Discount: editForm.Discount,
